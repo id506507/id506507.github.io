043a607f6c470babd2b04d779c2b5f855ffc0856
Chan Tsz Ching <id506507@gmail.com>
2022-01-21 15:40:17 +0800

    new verson webiste using Nuxt.js

---
+++ _nuxt/static/1642750598/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"personalWebsite\u002F_nuxt\u002Fstatic\u002F1642750598",layout:"default",error:a,state:{lang:"en"},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"personalWebsite\u002F",assetsPath:"personalWebsite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
