9cb03f8ac2b5e77d44ea0fcd746b80bd74cc3bea
Chan Tsz Ching <id506507@gmail.com>
2026-01-27 21:47:41 +0800

    update exitJw page, add screenshot

--- sourceCode/pages/exitJw.vue
+++ sourceCode/pages/exitJw.vue
@@ -1,19 +1,81 @@
 <template>
   <div class="outer">
-    <div>
-      <span
-        >我離開絕不是因為不良交往、不道德、貪財這些原因，而是因為耶和華見證人(下稱
-        組織)的某些說法。大致有以下3個原因：</span
-      >
-      <ol>
-        <li>組織的教義會不斷調整</li>
-        <li>中央長老團是受神聖力量指引</li>
-        <li>真理之光真越照越亮</li>
-      </ol>
-    </div>
-
     <div class="param">
-      先在這裡定義一下何謂真理。真理是不可能改變，而且是可以被證明的。組織經常引用箴言4：18「但義人的路好像明亮的晨光，越來越亮，直到正午。」但縱觀整篇箴言第4章，其實這並不是在講述教義會愈來愈清晰，而是人行為的標準，就像19節的提到「惡人的路黑暗無光」。所以18段的經文並不是解釋為什麼教義會調整。
+      <span
+        >我離開絕不是因為不良交往、不道德、貪財這些原因，我想在這裡講一下我離開的原因。</span
+      >
+    </div>
+    <!-- point one 中央長老團-->
+    <div class="param">
+      <span class="subtitle">第一點</span
+      >，我認為中央長老團並非受神聖力量啟示。這裡引用一些組織出版物的資料。在<a
+        :href="getLink(11)"
+        target="_blank"
+        >《耶和華見證人——在普世團結一致地遵行上帝的旨意》</a
+      >這個冊子中的《團結一致地牧養上帝的羊群》指出「他們並非受上帝所感示，因此不是永不犯錯的，但是他們將上帝那萬無謬誤的話語視為世上最高的權威，全心信賴聖經的指引，並且在順服上帝的旨意方面積聚了一生的豐富經驗。」
+      <figure class="screenshot">
+        <img :src="getScreenshot(11)" />
+        <figcaption>{{ getCaption(11) }}</figcaption>
+      </figure>
+      然後，在2017年2月的守望台研讀版《今天誰正領導上帝的子民？》這篇文章，在<a
+        :href="getLink(9)"
+        target="_blank"
+        >中文版</a
+      >指出「今天的中央長老團成員都不完美，所以還是會在解釋聖經或提出組織的訓示上出錯。」這看起來並沒有什麼問題，因為大家都知道人是不完美的，所以出錯是一件很正常的事。不過在<a
+        :href="getLink(10)"
+        target="_blank"
+        >英文版</a
+      >卻指出「The Governing Body is neither inspired nor infallible. Therefore,
+      it can err in doctrinal matters or in organizational direction.」Google
+      翻譯後的結果就是「中央長老團既非受聖靈啟示，也非絕對正確。因此，它在教義問題或組織方向上都可能犯錯。」這並不是說中央長老團不完美有錯。可以回想耶和華以前也任用了不完美的人去寫下不少預言，甚至是聖經。但這些人所寫下的東西是完全正確，但文章卻說在解釋聖經或提出組織的訓示會出錯，這不是有問題嗎？
+      <figure class="screenshot">
+        <img :src="getScreenshot(9)" />
+        <figcaption>{{ getCaption(9) }}</figcaption>
+      </figure>
+      我們每個人都知道中央長老團是忠信睿智的奴隸，是耶和華所任用的，受上帝神聖力量任命的基督徒。在<a
+        :href="getLink(13)"
+        target="_blank"
+        >《永遠享受美好的生命——互動式聖經課程中》</a
+      >就提到「同樣，今天也有一小群長老，正在為上帝的子民提供滋養信心的食物，督導傳道工作。他們是耶和華見證人的中央長老團。這群長老就是受耶穌委派的「忠信睿智的奴隸」。中央長老團的所有成員都是受上帝神聖力量任命的基督徒。」所以如果中央長老團是受神聖力量任命，但又不是受神聖力量啟示，這不是很矛盾嗎？
+      <figure class="screenshot">
+        <img :src="getScreenshot(13)" />
+        <figcaption>{{ getCaption(13) }}</figcaption>
+      </figure>
+    </div>
+    <!-- point one 中央長老團-->
+
+    <!-- point two 真理越來越明亮 -->
+    <div class="param">
+      <span class="subtitle">第二點</span
+      >，真理越來越明亮。先在這裡定義一下何謂真理，真理就是真實、客觀且恆常不變的道理或規律。而我以前學到的就是真理是不可能改變，而且是可以被證明的。這裡想舉出三個例子。
+    </div>
+    <div class="param">
+      首先是額外教育。在1972年的警醒中<a :href="getLink(2)" target="_blank"
+        >《對大學教育的新評估》</a
+      >這篇文章都是對大學教育提出批評。
+      <figure class="screenshot">
+        <img :src="getScreenshot(2)" />
+        <figcaption>{{ getCaption(2) }}</figcaption>
+      </figure>
+      然後在1989年12月8日警醒中，在
+      <a :href="getLink(1)" target="_blank">《大學教育——真的有利嗎？》</a
+      >這個部份，文章表達大學教育會令人對信仰和道德造成影響。
+      <figure class="screenshot">
+        <img :src="getScreenshot(1)" />
+        <figcaption>{{ getCaption(1) }}</figcaption>
+      </figure>
+      但是在1992年11月1日的守望台的其中一篇文章<a
+        :href="getLink(3)"
+        target="_blank"
+        >《懷着目的而受教育》</a
+      >卻說要對教育懷有平衡的看法。文章提到「完成最低程度的教育或甚至完成中學教育均不足以使他們有適當的收入去維持自己作先驅，他們也許可以考慮接受額外的教育或職業訓練」。因此接受額外的教育可以幫助人們從事全時服務。
+      <figure class="screenshot">
+        <img :src="getScreenshot(3)" />
+        <figcaption>{{ getCaption(3) }}</figcaption>
+      </figure>
+    </div>
+    <div class="param">
+      組織經常引用箴言4：18「但義人的路好像明亮的晨光，越來越亮，直到正午。」但縱觀整篇箴言第4章，其實這並不是在講述教義會愈來愈清晰，而是人行為的標準，就像19節的提到「惡人的路黑暗無光」。所以18段的經文並不是解釋為什麼教義會調整。
     </div>
 
     <div class="param">
@@ -21,17 +83,7 @@
     </div>
 
     <div class="param">
-      首先，是額外教育。在1989年12月8日警醒中，在
-      <a :href="getLink(1)" target="_blank">《大學教育——真的有利嗎？》</a
-      >這個部份，文章表達大學教育會令人對信仰和道德造成影響，而在《大學教育以外的其他選擇》就提出了「耶和華見證人會衆所提供的訓練——尤以每週舉行的神治傳道訓練班為然——幫助他們在求職時具備優越的條件」，所以這篇文章就是視大學教育是一件負面的東西。在1972年的警醒中<a
-        :href="getLink(2)"
-        target="_blank"
-        >《對大學教育的新評估》</a
-      >這篇文章也是有同樣的說法，其中一個次標題甚至為《不適當的課程》。但是在1992年11月1日的守望台的其中一篇文章<a
-        :href="getLink(3)"
-        target="_blank"
-        >《懷着目的而受教育》</a
-      >卻說要對教育懷有平衡的看法。文章也提到「在許多地方，人若僅是完成法律所規定的最低程度的學校教育（在有些國家裏，甚至達到中學畢業的程度），就很難找着薪酬適當的工作。」，因此接受額外的教育可以幫助人們從事全時服務。另外在1999年9月1日守望台的其中一篇文章<a
+      首先，是額外教育。另外在1999年9月1日守望台的其中一篇文章<a
         :href="getLink(4)"
         target="_blank"
         >《青年人，要鍛鍊你的了解力！》</a
@@ -56,24 +108,6 @@
 
     <div class="param">
       這看起來是一件好事，因為現在允許額外教育和器官移植。但重點是為什麼這些觀點會不斷改變，甚至回到之前的立場。如果這是真理、是耶和華所指引的，這些教義會不斷改變嗎？耶和華給的指引會不斷改變嗎？在瑪拉基書3：6指出，耶和華從沒有改變，因此我相信耶和華所提供的指引也是非常清晰，不會過了一段時間就需要作出改變。
-    </div>
-
-    <div class="param">
-      因此，可以看看中央長老團。我們每個人都知道中央長老團是忠信睿智的奴隸，是耶和華所任用的，受神聖力量指引的。在2017年2月的守望台研讀版《今天誰正領導上帝的子民？》這篇文章，在<a
-        :href="getLink(9)"
-        target="_blank"
-        >中文版</a
-      >指出「今天的中央長老團成員都不完美，所以還是會在解釋聖經或提出組織的訓示上出錯。」這看起來並沒有什麼問題，因為大家都知道人是不完美的，所以出錯是一件很正常的事。不過在<a
-        :href="getLink(10)"
-        target="_blank"
-        >英文版</a
-      >卻指出「The Governing Body is neither inspired nor infallible. Therefore,
-      it can err in doctrinal matters or in organizational direction.」Google
-      翻譯後的結果就是「中央長老團既非受聖靈啟示，也非絕對正確。因此，它在教義問題或組織方向上都可能犯錯。」而且在<a
-        :href="getLink(11)"
-        target="_blank"
-        >《耶和華見證人——在普世團結一致地遵行上帝的旨意》</a
-      >這個冊子中的《團結一致地牧養上帝的羊群》也指出「他們並非受上帝所感示，因此不是永不犯錯的，但是他們將上帝那萬無謬誤的話語視為世上最高的權威，全心信賴聖經的指引，並且在順服上帝的旨意方面積聚了一生的豐富經驗。」所以如果中央長老團是受神聖力量指引，但又不是受神聖力量啟示，這不是很矛盾嗎？這並不是說中央長老團不完美有錯。可以回想耶和華以前也任用了不完美的人去寫下不少預言，甚至是聖經。但這些人所寫下的東西是完全正確，但文章卻說在解釋聖經或提出組織的訓示會出錯，這不是有問題嗎？
     </div>
 
     <div class="param">
@@ -119,7 +153,7 @@ export default Vue.extend({
         },
         {
           index: 3,
-          href: 'https://wol.jw.org/yue-Hant/wol/d/r214/lp-chc/1992804#:~:text=%E5%9C%A8%E8%A8%B1%E5%A4%9A%E5%9C%B0%E6%96%B9%EF%BC%8C%E4%BA%BA%E8%8B%A5%E5%83%85%E6%98%AF%E5%AE%8C%E6%88%90%E6%B3%95%E5%BE%8B%E6%89%80%E8%A6%8F%E5%AE%9A%E7%9A%84%E6%9C%80%E4%BD%8E%E7%A8%8B%E5%BA%A6%E7%9A%84%E5%AD%B8%E6%A0%A1%E6%95%99%E8%82%B2%EF%BC%88%E5%9C%A8%E6%9C%89%E4%BA%9B%E5%9C%8B%E5%AE%B6%E8%A3%8F%EF%BC%8C%E7%94%9A%E8%87%B3%E9%81%94%E5%88%B0%E4%B8%AD%E5%AD%B8%E7%95%A2%E6%A5%AD%E7%9A%84%E7%A8%8B%E5%BA%A6%EF%BC%89%EF%BC%8C%E5%B0%B1%E5%BE%88%E9%9B%A3%E6%89%BE%E7%9D%80%E8%96%AA%E9%85%AC%E9%81%A9%E7%95%B6%E7%9A%84%E5%B7%A5%E4%BD%9C%E3%80%82',
+          href: 'https://wol.jw.org/yue-Hant/wol/d/r214/lp-chc/1992804#:~:text=%E5%AE%8C%E6%88%90%E6%9C%80%E4%BD%8E%E7%A8%8B%E5%BA%A6%E7%9A%84%E6%95%99%E8%82%B2%E6%88%96%E7%94%9A%E8%87%B3%E5%AE%8C%E6%88%90%E4%B8%AD%E5%AD%B8%E6%95%99%E8%82%B2%E5%9D%87%E4%B8%8D%E8%B6%B3%E4%BB%A5%E4%BD%BF%E4%BB%96%E5%80%91%E6%9C%89%E9%81%A9%E7%95%B6%E7%9A%84%E6%94%B6%E5%85%A5%E5%8E%BB%E7%B6%AD%E6%8C%81%E8%87%AA%E5%B7%B1%E4%BD%9C%E5%85%88%E9%A9%85%EF%BC%8C%E4%BB%96%E5%80%91%E4%B9%9F%E8%A8%B1%E5%8F%AF%E4%BB%A5%E8%80%83%E6%85%AE%E6%8E%A5%E5%8F%97%E9%A1%8D%E5%A4%96%E7%9A%84%E6%95%99%E8%82%B2%E6%88%96%E8%81%B7%E6%A5%AD%E8%A8%93%E7%B7%B4',
         },
         {
           index: 4,
@@ -157,12 +191,49 @@ export default Vue.extend({
           index: 12,
           href: 'https://www.jw.org/yue-hant/%E5%A4%9A%E5%AA%92%E9%AB%94%E5%9C%96%E6%9B%B8%E9%A4%A8/%E9%9B%9C%E8%AA%8C/%E5%AE%88%E6%9C%9B%E5%8F%B0%E7%A0%94%E8%AE%80%E7%89%882026%E5%B9%B42%E6%9C%88/%E8%AE%80%E8%80%85%E4%BE%86%E5%87%BD/',
         },
+        {
+          index: 13,
+          href: 'https://wol.jw.org/yue-Hant/wol/d/r214/lp-chc/1102021254#:~:text=%E5%90%8C%E6%A8%A3%EF%BC%8C%E4%BB%8A%E5%A4%A9%E4%B9%9F%E6%9C%89%E4%B8%80%E5%B0%8F%E7%BE%A4%E9%95%B7%E8%80%81%EF%BC%8C%E6%AD%A3%E5%9C%A8%E7%82%BA%E4%B8%8A%E5%B8%9D%E7%9A%84%E5%AD%90%E6%B0%91%E6%8F%90%E4%BE%9B%E6%BB%8B%E9%A4%8A%E4%BF%A1%E5%BF%83%E7%9A%84%E9%A3%9F%E7%89%A9%EF%BC%8C%E7%9D%A3%E5%B0%8E%E5%82%B3%E9%81%93%E5%B7%A5%E4%BD%9C%E3%80%82%E4%BB%96%E5%80%91%E6%98%AF%E8%80%B6%E5%92%8C%E8%8F%AF%E8%A6%8B%E8%AD%89%E4%BA%BA%E7%9A%84%E4%B8%AD%E5%A4%AE%E9%95%B7%E8%80%81%E5%9C%98%E3%80%82%E9%80%99%E7%BE%A4%E9%95%B7%E8%80%81%E5%B0%B1%E6%98%AF%E5%8F%97%E8%80%B6%E7%A9%8C%E5%A7%94%E6%B4%BE%E7%9A%84%E3%80%8C%E5%BF%A0%E4%BF%A1%E7%9D%BF%E6%99%BA%E7%9A%84%E5%A5%B4%E9%9A%B8%E3%80%8D%E3%80%82%EF%BC%88%E9%A6%AC%E5%A4%AA%E7%A6%8F%E9%9F%B324%3A45%E4%B8%8A%E5%8D%8A%E9%83%A8%E5%88%86%EF%BC%89%E4%B8%AD%E5%A4%AE%E9%95%B7%E8%80%81%E5%9C%98%E7%9A%84%E6%89%80%E6%9C%89%E6%88%90%E5%93%A1%E9%83%BD%E6%98%AF%E5%8F%97%E4%B8%8A%E5%B8%9D%E7%A5%9E%E8%81%96%E5%8A%9B%E9%87%8F%E4%BB%BB%E5%91%BD%E7%9A%84%E5%9F%BA%E7%9D%A3%E5%BE%92',
+        },
+      ],
+      caption: [
+        {
+          index: 11,
+          text: '來自《耶和華見證人——在普世團結一致地遵行上帝的旨意》的截圖',
+        },
+        {
+          index: 9,
+          text: '來自《今天誰正領導上帝的子民？》的中英文版截圖',
+        },
+        {
+          index: 13,
+          text: '來自《永遠享受美好的生命——互動式聖經課程中》的截圖',
+        },
+        {
+          index: 2,
+          text: '來自《對大學教育的新評估》的截圖，每個分標題都是對大學教育的批評',
+        },
+        {
+          index: 1,
+          text: '來自1989年12月8日警醒的部份截圖',
+        },
+        {
+          index: 3,
+          text: '來自《懷着目的而受教育》的截圖',
+        },
       ],
     }
   },
   methods: {
     getLink(index: number) {
       return this.link.find((f: { index: number }) => f.index === index)?.href
+    },
+    getScreenshot(index: number) {
+      return `Screenshot_${index}.png`
+    },
+    getCaption(index: number) {
+      return this.caption.find((f: { index: number }) => f.index === index)
+        ?.text
     },
   },
   mounted() {
@@ -193,5 +264,19 @@ export default Vue.extend({
   margin-bottom: 2rem;
   letter-spacing: 2px;
   line-height: 1.6;
+}
+.screenshot {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border-style: dashed;
+}
+.screenshot > img {
+  width: 100%;
+}
+figcaption {
+  font-size: small;
+}
+.subtitle {
+  color: blue;
 }
 </style>
